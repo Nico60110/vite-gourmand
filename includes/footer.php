15b56d4b4b1a7efd4688e81fac269ce38c51e4ce
Nico60110 <nicolas.gerard1907@gmail.com>
2026-05-15 17:37:29 +0200

    Create navbar and footer with responsive

---
+++ includes/footer.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>footer</title>
+    <link rel="stylesheet" href="../css/vite&gourmand.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+ <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;700&display=swap" rel="stylesheet">
+<body>
+    <nav></nav>
+    
+    
+</head>
+<body>
+    <footer class="footer">
+    <div class="footer-top">
+
+        <!-- Colonne gauche -->
+        <div class="footer-brand">
+            <img src="../images/chapeau-de-chef.png " alt="Logo Vite & Gourmand" class="footer-logo">
+
+            <h1>Vite &<br>Gourmand</h1>
+
+            <p class="subtitle">Traiteur à Bordeaux</p>
+
+            <p class="description">
+                Des menus gourmands et raffinés pour tous vos événements
+            </p>
+        </div>
+
+        <!-- Colonne centre -->
+        <div class="footer-hours">
+            <div class="hours-chevron">
+                 <h3><i class="bi bi-clock"></i> HORAIRES D’OUVERTURE</h3>
+                <i class="bi bi-chevron-down"></i>   
+            </div>
+            
+
+            <div class="hours-list">
+                <span>Lundi</span><span>9h - 18h</span>
+                <span>Mardi</span><span>9h - 18h</span>
+                <span>Mercredi</span><span>9h - 18h</span>
+                <span>Jeudi</span><span>9h - 18h</span>
+                <span>Vendredi</span><span>9h - 18h</span>
+                <span>Samedi</span><span>9h - 18h</span>
+                <span>Dimanche</span><span>Fermé</span>
+            </div>
+        </div>
+
+        <!-- Colonne droite -->
+        <div class="footer-links">
+            <div class="link-chevron">
+                <h3><i class="bi bi-link-45deg"></i> LIEN UTILES</h3>
+            <i class="bi bi-chevron-down"></i>
+        </div>
+            
+
+            <ul>
+                <li><a href="#">Accueil</a></li>
+                <li><a href="#">Nos menus</a></li>
+                <li><a href="#">Inscription</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </div>
+
+    </div>
+
+    <!-- Ligne du bas -->
+    <div class="footer-bottom">
+
+        <p>© 2026 Vite & Gourmand</p>
+
+        <div class="footer-center-logo">
+            <div class="line"></div>
+
+            <img src="../images/chapeau-de-chef.png">
+
+            <div class="line"></div>
+        </div>
+
+        <p>Mentions légales | CGV</p>
+
+    </div>
+</footer>
+    
+</body>
+</html>
---
+++ includes/navbar.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>navbar</title>
+</head>
+
+<link rel="stylesheet" href="../css/vite&gourmand.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+ <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+ <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;700&display=swap" rel="stylesheet">
+<body>
+    <nav>
+        <div class="logo">
+            <i class="bi bi-list"></i>
+            <span class='image'><img src="../images/chapeau-de-chef.png" alt=""></span>
+            <div class="titre"> 
+                
+                
+                <h2>Vite &<br>Gourmand</h2>
+                <p>Traiteur à bordeaux</p>
+            </div>
+        </div>
+           
+        
+        <ul class="liens1">
+            <li>Accueil</li>
+            <li>Nos menus</li>
+            <li>Inscription</li>
+            <li>Contact</li>
+        </ul>
+        <ul class="liens2">
+            <i class="bi bi-person"></i><li></i>Mon compte</li>
+            <i class="bi bi-handbag"></i><li>Panier</li>
+        </ul>
+    </nav>
+    
+</body>
+</html>
