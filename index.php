d958d3d09173cb8db506d97eb473e5291d5cb66c
Nico60110 <nicolas.gerard1907@gmail.com>
2026-05-17 01:46:53 +0200

    Create homepage layout

---
+++ index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <title>Vite & Gourmand</title>
+
+  <link rel="stylesheet" href="css/accueil.css">
+   </head>
+
+<body>
+
+  <!-- HERO -->
+  <section class="hero">
+    <div class="hero-content">
+      
+
+      <h1>Vite & Gourmand</h1>
+
+      <div class="subtitle">
+        traiteur • bordeaux depuis 25 ans
+      </div>
+
+      <p>
+        Des menus <strong>faits maison</strong> avec des produits frais
+        pour tous vos événements
+      </p>
+
+      <a href="#" class="btn">
+        Découvrir nos menus →
+      </a>
+
+    </div>
+  </section>
+
+  <!-- FEATURES -->
+  <section class="features">
+
+    <div class="feature">
+      <div class="icon-circle">🏅</div>
+      <strong>25 ans</strong>
+      <p>d’expérience</p>
+    </div>
+
+    <div class="feature">
+      <div class="icon-circle">🍃</div>
+      <strong>Produits</strong>
+      <p>frais</p>
+    </div>
+
+    <div class="feature">
+      <div class="icon-circle">👨‍🍳</div>
+      <strong>Équipe</strong>
+      <p>passionnée</p>
+    </div>
+
+    <div class="feature">
+      <div class="icon-circle">🛡️</div>
+      <strong>Engagement</strong>
+      <p>qualité</p>
+    </div>
+
+  </section>
+
+  <!-- TESTIMONIALS -->
+  <section class="testimonials">
+
+    <h2>Avis de nos clients</h2>
+
+    <div class="testimonial-grid">
+
+      <div class="card">
+        
+      </div>
+
+      <div class="card">
+       
+      </div>
+
+      <div class="card">
+        
+      </div>
+
+    </div>
+
+  </section>
+
+</body>
+</html>
