f01095a3a13d323aa21de81771cf4a3665b181fb
Nico60110 <nicolas.gerard1907@gmail.com>
2026-05-19 22:47:38 +0200

    Create register page

---
+++ pages/inscription.php
@@ -0,0 +1,174 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Inscription - Vite & Gourmand</title>
+    <link rel="stylesheet" href="../css/inscription.css">
+
+    <!-- GOOGLE FONT -->
+
+    <link
+        href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@500;600&family=Roboto:wght@400;500&display=swap"
+        rel="stylesheet">
+
+
+</head>
+
+<body>
+
+    <!-- =========================
+         CONTAINER
+    ========================= -->
+
+    <main class="register-container">
+
+        <!-- TOP -->
+
+        <section class="register-top">
+
+            <!-- IMAGE -->
+
+            <div class="register-image">
+
+                <img src="https://images.unsplash.com/photo-1547592180-85f173990554?q=80&w=1400"
+                    alt="Plat gastronomique">
+
+                <div class="overlay">
+
+                    <h1>Créer mon compte</h1>
+
+                    <p>
+                        Créer votre compte pour commander vos
+                        menus traiteur en quelques clics et suivre
+                        vos commandes.
+                    </p>
+
+                </div>
+
+            </div>
+
+            <!-- FORM -->
+
+            <div class="form-container">
+
+                <!-- INFOS -->
+
+                <div>
+
+                    <h2 class="section-title">
+                        Informations personnelles
+                    </h2>
+
+                    <div class="form-grid">
+
+                        <input type="text" placeholder="Prenom">
+
+                        <input type="text" placeholder="Nom">
+
+                        <input type="email" placeholder="Email" class="full-width">
+
+                        <input type="tel" placeholder="Téléphone" class="full-width">
+
+                        <input type="date" class="full-width">
+
+                    </div>
+
+                </div>
+
+                <!-- SECURITY -->
+
+                <div>
+
+                    <h2 class="section-title">
+                        Sécurité
+                    </h2>
+
+                    <div class="security-box">
+
+                        <input type="password" placeholder="Mot de passe">
+
+                        <input type="password" placeholder="Confirmer mot de passe">
+
+                        <button class="btn">
+                            Créer mon compte →
+                        </button>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </section>
+
+        <!-- FEATURES -->
+
+        <section class="features">
+
+            <!-- FEATURE -->
+
+            <div class="feature">
+
+                <div class="icon">👜</div>
+
+                <div>
+
+                    <h3>Commande rapide</h3>
+
+                    <p>
+                        Gagnez du temps avec vos informations
+                        enregistrées
+                    </p>
+
+                </div>
+
+            </div>
+
+            <!-- FEATURE -->
+
+            <div class="feature">
+
+                <div class="icon">☑</div>
+
+                <div>
+
+                    <h3>Suivie de commande</h3>
+
+                    <p>
+                        Suivez l’état de vos commandes en temps réel
+                        jusqu’à la livraison ou au retrait
+                    </p>
+
+                </div>
+
+            </div>
+
+            <!-- FEATURE -->
+
+            <div class="feature">
+
+                <div class="icon">♡</div>
+
+                <div>
+
+                    <h3>Historique</h3>
+
+                    <p>
+                        Retrouvez toutes vos commandes et
+                        factures facilement
+                    </p>
+
+                </div>
+
+            </div>
+
+        </section>
+
+    </main>
+
+</body>
+
+</html>
