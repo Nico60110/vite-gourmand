3801a8fd610cc34606ebf1fdc504af8f0e71fcb6
Nico60110 <nicolas.gerard1907@gmail.com>
2026-05-25 19:45:49 +0200

    Create register system

--- pages/inscription.php
+++ pages/inscription.php
@@ -1,3 +1,85 @@
+<?php
+
+require '../config/database.php';
+
+if($_SERVER["REQUEST_METHOD"] === "POST"){
+
+    $firstname = $_POST['firstname'];
+
+    $lastname = $_POST['lastname'];
+
+    $email = $_POST['email'];
+
+    $password = $_POST['password'];
+
+    $confirmPassword = $_POST['confirm_password'];
+
+
+
+    if(
+    !preg_match('/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{10,}$/',$password))
+    {
+        echo "Mot de passe non sécurisé";
+    }
+    elseif($password !== $confirmPassword){
+
+    echo "Les mots de passe ne correspondent pas";
+
+    }
+    else {
+
+        // HASH PASSWORD
+
+        $hashedPassword = password_hash(
+            $password,
+            PASSWORD_DEFAULT
+        );
+
+        // ROLE PAR DEFAUT
+
+        $idRole = 3;
+
+        // VERIFICATION EMAIL
+
+        $checkEmail = $pdo->prepare(
+            "SELECT * FROM utilisateur WHERE email = ?"
+        );
+
+        $checkEmail->execute([$email]);
+
+        $user = $checkEmail->fetch();
+
+        // SI EMAIL EXISTE
+
+        if($user){
+
+            echo "Email déjà utilisé";
+
+        } else {
+
+            // INSERT SQL
+
+            $sql = "INSERT INTO utilisateur
+            (prenom, nom, email, motDePasse, idRole)
+            VALUES (?, ?, ?, ?, ?)";
+
+            $query = $pdo->prepare($sql);
+
+            $query->execute([
+                $firstname,
+                $lastname,
+                $email,
+                $hashedPassword,
+                $idRole
+            ]);
+
+            header("Location: login.php");
+            exit;
+        }
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 
@@ -18,6 +100,7 @@
 </head>
 
 <body>
+    
 
     <!-- =========================
          CONTAINER
@@ -52,7 +135,7 @@
 
             <!-- FORM -->
 
-            <div class="form-container">
+            <form method="POST" class="form-container">
 
                 <!-- INFOS -->
 
@@ -64,13 +147,13 @@
 
                     <div class="form-grid">
 
-                        <input type="text" placeholder="Prenom">
+                        <input type="text" name="firstname" placeholder="Prenom">
 
-                        <input type="text" placeholder="Nom">
+                        <input type="text"  name="lastname" placeholder="Nom">
 
-                        <input type="email" placeholder="Email" class="full-width">
+                        <input type="email"  name="email" placeholder="Email" class="full-width">
 
-                        <input type="tel" placeholder="Téléphone" class="full-width">
+                        <input type="tel"  name="phone" placeholder="Téléphone" class="full-width">
 
                         <input type="date" class="full-width">
 
@@ -88,19 +171,19 @@
 
                     <div class="security-box">
 
-                        <input type="password" placeholder="Mot de passe">
+                        <input type="password"  name="password" placeholder="Mot de passe">
 
-                        <input type="password" placeholder="Confirmer mot de passe">
+                        <input type="password"  name="confirm_password" placeholder="Confirmer mot de passe">
 
-                        <button class="btn">
+                       <button type="submit" class="btn">
                             Créer mon compte →
-                        </button>
+                       </button>
 
                     </div>
 
                 </div>
 
-            </div>
+            </form>
 
         </section>
 
